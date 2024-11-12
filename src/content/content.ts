5ea44d6f1979c0827617b6f77c36613833afc919
yangzhou <yearrow@qq.com>
2024-11-12 17:10:23 +0800

    feat: 依赖插件安装

--- src/content/content.ts
+++ src/content/content.ts
@@ -2,3 +2,23 @@
 console.log('this is content page')
 console.log('this is content page')
 
+
+console.log("this is content js")
+const init = () => {
+  const addIframe = (id: string, pagePath: string) => {
+    const contentIframe = document.createElement("iframe");
+    contentIframe.id = id;
+    contentIframe.style.cssText = "width: 100%; height: 100%; position: fixed; inset: 0px; margin: 0px auto; z-index: 10000002; border: none;";
+    const getContentPage = chrome.runtime.getURL(pagePath);
+    contentIframe.src = getContentPage;
+    document.body.appendChild(contentIframe);
+  }
+
+  addIframe('content-start-iframe', 'contentPage/index.html')
+}
+
+// 判断 window.top 和 self 是否相等，如果不相等，则不注入 iframe
+if (window.top === window.self) {
+  // init();
+}
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from "@vitejs/plugin-vue-jsx";
-
+import path from 'path'
 import {crx} from '@crxjs/vite-plugin'
 import manifest from './manifest.json' // assert {type: 'json'} // Node >=17
 // https://vite.dev/config/
 export default defineConfig({
+  root: 'src/',
   plugins: [vue(), vueJsx(), crx({manifest})],
+
+  /*build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: {
+        contentPage: path.resolve(__dirname, 'src/contentPage/index.html'),
+        popup: path.resolve(__dirname, 'src/popup/index.html')
+      },
+      output: {
+        assetFileNames: 'assets/[name]-[hash].[ext]', // 静态资源
+        chunkFileNames: 'js/[name]-[hash].js', // 代码分割中产生的 chunk
+        entryFileNames: 'js/[name]-[hash].js'
+      }
+    }
+  }*/
 })
