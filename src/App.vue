c4410b2bf5475fed841d119982c9292d88581be7
yangzhou <yearrow@qq.com>
2024-11-12 16:48:02 +0800

    feat: add element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+const handleClick = () => {
+  console.log('handleClick');
+}
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <el-button type="primary" @click="handleClick">测试按钮</el-button>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
